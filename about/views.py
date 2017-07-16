c181949e439d8e723103b646888a785015e51323
JordanTFA <Jordantfa@hotmail.co.uk>
2017-07-16 05:58:44 +0100

    Got the collectstatic working

--- about/views.py
+++ about/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse('About page')
+	return render(request, 'about/index.html', {'context': "context"})
