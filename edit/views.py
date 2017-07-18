5fb63aae6988ba18800d02098d94df4a6ef87c16
JordanTFA <Jordantfa@hotmail.co.uk>
2017-07-18 10:50:51 +0100

    Adjustments
    Started work on Edit page

--- edit/views.py
+++ edit/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse('Edit Page')
+	return render(request, 'edit/edit.html', {'context': "context"})
